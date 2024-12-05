79a484fd8fd3ff92eb9aef31bc8251e2c8a12b64
Vivian <viviancroquet08@gmail.com>
2024-12-05 15:46:51 +0100

    pages d'inscription

---
+++ pages/inscription.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('./include/head.html'); ?>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <?php include('./include/header.php'); ?>
+
+    <h1>S'inscrire</h1>
+
+
+
+    <!-- inclusion du bas de page du site -->
+    <?php include('./include/footer.php'); ?>
+
+</body>
+
+</html>
