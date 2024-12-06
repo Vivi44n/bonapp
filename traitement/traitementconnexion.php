83250cf917e1bc4fa853cfc652832b0b7bbb1161
Vivian <viviancroquet08@gmail.com>
2024-12-06 16:38:05 +0100

    traitement connexion

--- traitement/traitementconnexion.php
+++ traitement/traitementconnexion.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset ($_POST [ 'identifient'] ) && !empty ($_POST [ 'identifient'] ) 
+if (isset ($_POST [ 'identifiant'] ) && !empty ($_POST [ 'identifiant'] ) 
 // && !empty ($_POST [ 'password'] ) && isset ($_POST [ 'password'] )
 ) {
 	
@@ -11,11 +11,11 @@ if (isset ($_POST [ 'identifient'] ) && !empty ($_POST [ 'identifient'] )
 
 $sql = "SELECT id_user, nom, mdp FROM users WHERE nom=:Bernard";
 $result = $pdo->prepare($sql);
-$result->execute(["Bernard"=>$_POST['identifient']]);
+$result->execute(["Bernard"=>$_POST['identifiant']]);
 $usersdata = $result->fetchAll();
 if (sizeof($usersdata) > 0 && isset($usersdata)) {
 	foreach ($usersdata as $users) {
-		if ($_POST["identifient"] === $users["nom"]) {
+		if ($_POST["identifiant"] === $users["nom"]) {
 			if ($users["mdp"] === $_POST["password"]) 
 			header ("Location: accueil");{
 			
