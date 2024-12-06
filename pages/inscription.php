585a01c8067c3f7e9111949e63577ab358e2ce6e
Vivian <viviancroquet08@gmail.com>
2024-12-06 13:41:50 +0100

    la page d'inscription fonctionne, quand j'ajoute une personne cela s'inscrit dans la bdd

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,14 +5,51 @@
 
 <body class="d-flex flex-column min-vh-100">
 
+<?php include('./traitement/traitementinscription.php'); ?>
+
     <?php include('./include/header.php'); ?>
 
     <h1>S'inscrire</h1>
+
+    <?php if (!isset($inscripUser  )) : ?>
+    <form action="inscription" method="post">
+        <!-- si message d'erreur on l'affiche -->
+        <?php if (isset($errorMessage)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo htmlspecialchars($errorMessage); ?>
+            </div>
+        <?php endif; ?>
+        <div class="mb-4">
+            <label for="identifiant" class="form-label">Identifiant</label>
+            <input type="text" class="form-control" id="identifiant" name="identifiant" aria-describedby="identifiantHelp" placeholder="Veuillez entrer un identifiant" required>
+        </div>
+        <div class="mb-4">
+            <label for="email" class="form-label">Adresse e-mail</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Veuillez entrer une adresse e-mail" required>
+        </div>
+        <div class="mb-4">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control" id="password" name="mdp" aria-describedby="passwordHelp" required>
+        </div>    
+
+<div class="mb-4">
+<input type="hidden" name="hid" value="true">
+
+
+
+
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+
+<?php     echo($erreur);
+endif;
+?>
 
 
 
     <!-- inclusion du bas de page du site -->
     <?php include('./include/footer.php'); ?>
+
 
 </body>
 
