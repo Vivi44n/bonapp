d42ce79849ddc1ccf3a269942681b73777ab4a59
Vivian <viviancroquet08@gmail.com>
2024-12-06 13:40:07 +0100

    contact fonctionne parfaitement

--- pages/contact.php
+++ pages/contact.php
@@ -16,21 +16,30 @@
     <?php if ($successMessage): ?>
         <p style="color: green;"><?= $successMessage; ?></p>
     <?php endif; ?>
+    <div class="nomemailmessage">
     <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="name">Nom:</label><br>
         <input type="text" id="name" name="name" value="<?= htmlspecialchars($name); ?>">
-        <span style="color: red;"><?= $nameErr; ?></span><br><br>
+        <span style="color: red;"><?= $nameErr; ?></span><br>
 
         <label for="email">Email:</label><br>
         <input type="text" id="email" name="email" value="<?= htmlspecialchars($email); ?>">
-        <span style="color: red;"><?= $emailErr; ?></span><br><br>
+        <span style="color: red;"><?= $emailErr; ?></span><br>
 
         <label for="message">Message:</label><br>
         <textarea id="message" name="message"><?= htmlspecialchars($message); ?></textarea>
-        <span style="color: red;"><?= $messageErr; ?></span><br><br>
+        <span style="color: red;"><?= $messageErr; ?></span>    <br>
+        </div>
 
-        <input type="submit" value="Envoyer">
-    </form>
+        <button type="submit" class="btn-primary">Envoyer</button>
+        <br>
+
+        </form>
+
+        <br>
+
+    <?php include('./include/footer.php'); ?>
+
 </body>
 </html>
 
