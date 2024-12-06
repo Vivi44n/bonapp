7e266f1b05bd7e1c9563fde69721473de0adfbe4
Vivian <viviancroquet08@gmail.com>
2024-12-06 16:35:52 +0100

    acccueil

--- pages/recettes/accueil.php
+++ pages/recettes/accueil.php
@@ -34,33 +34,7 @@ if (sizeof($recettes) > 0) {
     echo 'Aucune recette disponible.';
 }
 
-$recettes = [
-    [
-        "id_recette" => 1,
-        "nom" => "Lasagne à la bolognaise",
-        "image" => "assets\images\lasagnes.jpg"
-    ],
-    [
-        "id_recette" => 2,
-        "nom" => "Pot au feu",
-        "image" => "assets\images\potaufeu.jpg"
-    ],
-    [
-        "id_recette" => 3,
-        "nom" => "Crêpes",
-        "image" => "assets\images\crepe.jpg"
-    ],
-    [
-        "id_recette" => 4,
-        "nom" => "Steak Frite",
-        "image" => "images/steak_frite.jpg"
-    ],
-    [
-        "id_recette" => 5,
-        "nom" => "Œuf à la coque",
-        "image" => "images/oeuf_a_la_coque.jpg"
-    ],
-];
+
 ?>
 
 
